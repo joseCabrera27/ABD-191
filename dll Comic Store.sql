4bc41bdf3f758ad9a8162d17b0d0b2b9997b7744
joseCabrera27 <144623196+joseCabrera27@users.noreply.github.com>
2024-05-24 19:09:21 -0600

    Avance clase

---
+++ dll Comic Store.sql
@@ -0,0 +1,11 @@
+create table Clientes (
+id_clientes bigint identity(1,1) PRIMARY KEY,
+nombre varchar,
+correo_electronico varchar,
+pass varchar
+); 
+
+create table compras(
+id_compra bigint identity(1,1) PRIMARY KEY,
+
+);
